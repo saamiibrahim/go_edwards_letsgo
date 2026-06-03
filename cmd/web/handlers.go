75f3d209007491d25b3852c2210f662e819aaa03
Saami Ibrahim <saami.ibrahim.desaster@gmail.com>
2026-06-04 02:36:23 +0500

    Refactor snippet handling and add error management for snippets. Snippets Insert, Get and Latest Methods updated to get snipepts from DB and showing in respective handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"saami.github.com/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -13,44 +15,57 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
 	// Initialize a slice containing the paths to the two files. It's important
 	// to note that the file
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
+
+	// files := []string{
+	// 	"./ui/html/base.tmpl.html",
+	// 	"./ui/html/partials/nav.tmpl.html",
+	// 	"./ui/html/pages/home.tmpl.html",
+	// }
 
 	// Use the template.ParseFiles() function to read the files and store the
 	// templates in a template set. Notice that we can pass the slice of file
 	// paths as a variadic parameter?
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.serverError(w, err)
 
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standard logger.
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	app.serverError(w, err)
 
-		// app.errorLog.Print(err.Error())
-		// http.Error(w, "Internal Server Error", 500)
-		return
+	// Because the home handler function is now a method against application
+	// it can access its fields, including the error logger. We'll write the log
+	// message to this instead of the standard logger.
 
-	}
+	//old // app.errorLog.Print(err.Error())
+	//old // http.Error(w, "Internal Server Error", 500)
+	// 	return
+
+	// }
 
 	// We then use the Execute() method on the template set to write the
 	// template content as the response body. The last parameter to Execute()
 	// represents any dynamic data that we want to pass in, which for now we'll
 	// leave as nil.
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
 
-		app.serverError(w, err)
-		// Also update the code here to use the error logger from the application
-		// struct.
-		// app.errorLog.Print(err.Error())
-		// http.Error(w, "Internal Server Error", 500)
-	}
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+
+	// 	app.serverError(w, err)
+	//old // Also update the code here to use the error logger from the application
+	//old // struct.
+	//old // app.errorLog.Print(err.Error())
+	//old // http.Error(w, "Internal Server Error", 500)
+	// }
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -61,7 +76,22 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	// Use the SnippetModel object's Get method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response.
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
+	// fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
@@ -74,5 +104,22 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	// Create some variables holding dummy data. We'll remove these later on
+	// during the build.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	// Pass the data to the SnippetModel.Insert() method, receiving the
+	// ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+
+	// w.Write([]byte("Create a new snippet..."))
 }
